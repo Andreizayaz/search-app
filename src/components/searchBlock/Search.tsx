b02f41ea3671914ef1d7976c0997d0092e4f8c29
John Smoliak <poznavauka@mail.ru>
2022-07-30 20:39:50 +0300

    feat: add form tag to search block

--- src/components/searchBlock/Search.tsx
+++ src/components/searchBlock/Search.tsx
@@ -3,18 +3,20 @@ import { FC, ReactElement } from 'react';
 import { SEARCH_LABEL } from 'src/constants';
 
 export const Search: FC = (): ReactElement => (
-  <div className='input-group'>
-    <div id='search-autocomplete' className='form-outline'>
-      <i className='bi bi-search'></i>
-      <input
-        type='search'
-        id='search'
-        className='form-control'
-        placeholder='Search...'
-      />
+  <form>
+    <div className='input-group'>
+      <div id='search-autocomplete' className='form-outline'>
+        <i className='bi bi-search'></i>
+        <input
+          type='search'
+          id='search'
+          className='form-control'
+          placeholder='Search...'
+        />
+      </div>
+      <button type='button' className='btn btn-primary'>
+        {SEARCH_LABEL}
+      </button>
     </div>
-    <button type='button' className='btn btn-primary'>
-      {SEARCH_LABEL}
-    </button>
-  </div>
+  </form>
 );
